3fb24bcaadc72e09f8ca2c9081d9b67adfda5936
Robin <robin@kyanmedia.com>
2011-02-21 22:57:32 +0000

    Tank aiming + arrow

--- tank.js
+++ tank.js
@@ -2,17 +2,55 @@ function Tank(color) {
 	
 	this.color = color || 'hsl('+Math.random*255+',100,50)';
 	this.pos = parseInt(Math.random()*canvas.width,10);
+	this.angle = 0;
+	this.power = 50;
+	this.recharging = false;
+	this.rechargeTime = 5000;
+	
+	this.update = function(){
+		if (aimLeft) this.angle = Math.max(this.angle-1,-90);
+		if (aimRight) this.angle = Math.min(this.angle+1,90);
+		if (powerUp) this.power = Math.min(this.power+2,100);
+		if (powerDown) this.power = Math.max(this.power-2,0);
+		if (fireShot) {
+			this.recharging = true;
+			fireShot = false;
+			setTimeout(function(){
+				arguments[0].recharging = false;
+			},this.rechargeTime,this);
+		}
+	}
 	
 	this.draw = function(){
-		c.fillStyle = this.color;
+		c.save();
 		c.translate(this.pos,landscape.getHeight(this.pos));
+		
+		// draw tank
+		c.fillStyle = this.color;
 		c.beginPath();
-		c.moveTo(-10,0);
-		c.lineTo(-8,-10);
-		c.lineTo(8,-10);
-		c.lineTo(10,0);
+		c.moveTo(-8,0);
+		c.lineTo(-6,-10);
+		c.lineTo(6,-10);
+		c.lineTo(8,0);
 		c.closePath();
 		c.fill();
+
+		// draw arrow
+		c.translate(0,-5);
+		c.rotate(this.angle*Math.PI/180);
+		c.fillStyle = this.recharging ? 'rgba(255,0,0,0.7)' : 'rgba(0,255,0,0.7)';
+		c.beginPath();
+		c.moveTo(0,-12);
+		c.lineTo(-6,-12);
+		c.lineTo(-6,-22-this.power);
+		c.lineTo(-12,-22-this.power);
+		c.lineTo(0,-42-this.power);
+		c.lineTo(12,-22-this.power);
+		c.lineTo(6,-22-this.power);
+		c.lineTo(6,-12);
+		c.closePath();
+		c.fill();
+		
 		c.restore();
 	}
 	
--- webtanks.js
+++ webtanks.js
@@ -1,4 +1,10 @@
-var canvas, c, landscape;
+var canvas, c, landscape, loop;
+
+// tank state
+var aimLeft, aimRight, powerUp, powerDown, fireShot;
+
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
 
 function init(){
 	canvas = document.getElementsByTagName('canvas')[0];
@@ -10,7 +16,60 @@ function init(){
 	landscape.draw();
 	tank.draw();
 	
+	loop = setInterval(gameLoop,1000/30);
+	
+}
+
+function gameLoop(){
+	c.clearRect(0,0,canvas.width,canvas.height)
+	tank.update();
+	landscape.draw();
+	tank.draw();
 }
 
 
+function onKeyDown(e){
+	switch(e.keyCode){
+		case 27: // ESC
+			clearInterval(loop);
+		case 37: // left
+			aimLeft = true;
+			break;
+		case 39: // right
+			aimRight = true;
+			break;
+		case 38: // up
+			powerUp = true;
+			break;
+		case 40: // down
+			powerDown = true;
+			break;
+		case 32: // SPACE
+			fireShot = true;
+			break;
+		default:
+			break;
+	}
+}
 
+function onKeyUp(e){
+	switch(e.keyCode){
+		case 37:
+			aimLeft = false;
+			break;
+		case 39:
+			aimRight = false;
+			break;
+		case 38:
+			powerUp = false;
+			break;
+		case 40:
+			powerDown = false;
+			break;
+		case 32: // SPACE
+			fireShot = false;
+			break;
+		default:
+			break;
+	}
+}
